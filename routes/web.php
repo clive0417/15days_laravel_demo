1bdccda01e47a9f43c362b126c6e673e01d04f97
clive0417 <clive0417@gmail.com>
2020-08-28 23:08:09 +0800

    init back-up

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
 Route::get('/about', function () {
     return view('about');
