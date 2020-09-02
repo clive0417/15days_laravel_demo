d498f3840a09e356ad9ccc217bd1d04c5f30c166
clive0417 <clive0417@gmail.com>
2020-09-02 21:33:27 +0800

    update create form Routing

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,4 +13,16 @@ class PostController extends Controller
         return view('posts.index',['posts'=>$posts]);
 
     }
+
+    public function create() 
+    {
+        return view('posts.create');
+
+    } 
+
+    public function store(Request $request) // 用request 來承接create 所寫的資料 
+    {
+        //
+
+    } 
 }
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,26 @@
+<!--layouts. 的 . 代表資料夾-->
+@extends('layouts.app')
+
+@section('page_title')
+<section class="page-title">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4 class="text-uppercase">Create Post</h4>
+                        <ol class="breadcrumb">
+                            <li><a href="/">Home</a>
+                            </li>
+                            <li class="active"><a href="/posts">Blog</a>
+                            </li>
+                            <li class="active">Blog Listing</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+</section>
+@endsection
+
+@section('content')
+form
+@endsection
+            
